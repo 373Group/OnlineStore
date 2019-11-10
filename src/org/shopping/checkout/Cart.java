908059b03482f42d4700a11744d67df9a87f8293
jjpalanca <jjpalanca@email.arizona.edu>
2019-11-10 14:57:59 -0700

    Updated checkout package's class methods

--- src/org/shopping/checkout/Cart.java
+++ src/org/shopping/checkout/Cart.java
@@ -1,5 +1,19 @@
 package org.shopping.checkout;
+import java.util.ArrayList;
+import org.shopping.warehouse.*;
 
 public class Cart {
-
+	private ArrayList<Item> items;
+	
+	public double getTotal() {
+		return 000;
+	}
+	
+	public boolean tooFewItems() {
+		return false;
+	}
+	
+	public String SendReceipt() {
+		return "";
+	}
 }
--- src/org/shopping/checkout/PaymentMethod.java
+++ src/org/shopping/checkout/PaymentMethod.java
@@ -1,5 +1,18 @@
 package org.shopping.checkout;
+import java.util.ArrayList;
+import org.shopping.warehouse.*;
 
 public class PaymentMethod {
-
-}
+	private ArrayList<String> PaymentType;
+	public boolean CheckIfCorrect() {
+		return false;
+		
+	}
+	public void IncorrectPayment() {
+		
+	}
+	
+	public void CheckOut() {
+		
+	}
+}	
